cef1540c7622f7149a1b07e80f22337fa5638822
Cielquan <cielquan@protonmail.com>
2020-05-22 14:36:40 +0200

    added more tests

--- tests/test_check_jira_tag.py
+++ tests/test_check_jira_tag.py
@@ -29,41 +29,165 @@
     :copyright: (c) 2020 Christian Riedel
     :license: GPLv3, see LICENSE for more details
 """
-import configparser
-
+# pylint: disable=W0613,W0621
 from pathlib import Path
 
-import pytest
+import pytest  # type: ignore
 
 from click.testing import CliRunner
+from jira import JIRA  # type: ignore
+from jira import exceptions as jira_exc
 
 from commit_msg_jira_hook.check_jira_tag import main
 
 
-def test_missing_jira_url():
-    runner = CliRunner()
-    result = runner.invoke(main, ["--jira-tag=TAG", "--verify", "TESTFILE"])
+@pytest.fixture
+def cli_runner():
+    """Yield click cli test runner"""
+    yield CliRunner()
+
+
+@pytest.fixture
+def mock_home(monkeypatch, tmp_path):
+    """Yield `home` dir mock path"""
+    home = tmp_path / "~"
+    home.mkdir()
+    monkeypatch.setattr(Path, "home", lambda: home)
+    yield home
+
+
+@pytest.fixture
+def mock_ini(mock_home):
+    """Yield '.jira.ini' mock path in `home` dir mock"""
+    ini = mock_home / ".jira.ini"
+    ini.write_text("[jira]\nJIRA_USERNAME=USERNAME\nJIRA_TOKEN=TOKEN\n")
+    yield ini
+
+
+@pytest.fixture
+def mock_commit_msg_file(mock_home):
+    """Yield 'COMMIT_MSG' path in `home` dir mock"""
+    commit_msg_file = mock_home / "COMMIT_MSG"
+    yield commit_msg_file
+
+
+def test_missing_jira_url(cli_runner):
+    """Assert error on missing jira-url when `--verify`"""
+    result = cli_runner.invoke(main, ["--jira-tag=TAG", "--verify", "NO_FILE"])
     assert result.exit_code == 1
     assert "Please provide '--jira-url'" in result.output
 
 
-def test_missing_ini_file(monkeypatch):
-    monkeypatch.setattr(Path, "is_file", lambda _: False)
-    runner = CliRunner()
-    result = runner.invoke(
-        main, ["--jira-tag=TAG", "--jira-url=URL", "--verify", "TESTFILE"]
-    )
+def test_missing_ini_file(mock_home, cli_runner):
+    """Assert error on missing '~/.jira.ini' file"""
+    result = cli_runner.invoke(main, ["--jira-tag=TAG", "--jira-url=URL", "NO_FILE"])
     assert result.exit_code == 1
     assert "No '~/.jira.ini' file found." in result.output
 
 
-def test_missing_jira_section(monkeypatch):
-    monkeypatch.setattr(Path, "is_file", lambda _: True)
-    monkeypatch.setattr(configparser.ConfigParser, "read", lambda self, _: "")
-    # mock jira ini file
-    runner = CliRunner()
-    result = runner.invoke(
-        main, ["--jira-tag=TAG", "--jira-url=URL", "--verify", "TESTFILE"]
-    )
+def test_missing_jira_section(mock_home, cli_runner):
+    """Assert error on missing 'jira' section in '~/.jira.ini' file"""
+    mock_ini = mock_home / ".jira.ini"
+    mock_ini.write_text("")
+
+    result = cli_runner.invoke(main, ["--jira-tag=TAG", "--jira-url=URL", "NO_FILE"])
     assert result.exit_code == 1
     assert "No 'jira' section" in result.output
+
+
+def test_missing_conf_key(mock_home, cli_runner):
+    """Assert error on missing conf key in 'jira' section"""
+    mock_ini = mock_home / ".jira.ini"
+    mock_ini.write_text("[jira]\nJIRA_USERNAME=USERNAME\n")
+
+    result = cli_runner.invoke(main, ["--jira-tag=TAG", "--jira-url=URL", "NO_FILE"])
+
+    assert result.exit_code == 1
+    assert "Missing 'JIRA_TOKEN'" in result.output
+
+
+def test_empty_commit_msg(mock_ini, mock_commit_msg_file, cli_runner):
+    """Assert error on empty commit msg"""
+    mock_commit_msg_file.write_text("")
+
+    result = cli_runner.invoke(
+        main, ["--jira-tag=TAG", "--jira-url=URL", str(mock_commit_msg_file)]
+    )
+
+    assert result.exit_code == 1
+    assert "Commit message is empty." in result.output
+
+
+@pytest.mark.parametrize("commit_msg", ["message", "tag", "tag-", "#tag", "#tag-123"])
+def test_missing_tag(commit_msg, mock_ini, mock_commit_msg_file, cli_runner):
+    """Assert error on missing tag"""
+    mock_commit_msg_file.write_text(commit_msg)
+
+    result = cli_runner.invoke(
+        main, ["--jira-tag=TAG", "--jira-url=URL", str(mock_commit_msg_file)]
+    )
+
+    assert result.exit_code == 1
+    assert "'TAG' tag not found" in result.output
+
+
+@pytest.mark.parametrize("commit_msg", ["TAG", "TAG-", "#TAG", "#TAG-"])
+def test_missing_tag_number(commit_msg, mock_ini, mock_commit_msg_file, cli_runner):
+    """Assert error on missing tag number"""
+    mock_commit_msg_file.write_text(commit_msg)
+
+    result = cli_runner.invoke(
+        main, ["--jira-tag=TAG", "--jira-url=URL", str(mock_commit_msg_file)]
+    )
+
+    assert result.exit_code == 1
+    assert "'TAG' tag but no number" in result.output
+
+
+def test_jira_error(mock_ini, mock_commit_msg_file, monkeypatch, cli_runner):
+    """Assert error on jira api login"""
+    mock_commit_msg_file.write_text("TAG-123")
+
+    def mock_init(*args, **kwargs):
+        raise jira_exc.JIRAError()
+
+    monkeypatch.setattr(JIRA, "__init__", mock_init)
+
+    result = cli_runner.invoke(
+        main, ["--jira-tag=TAG", "--jira-url=URL", str(mock_commit_msg_file)]
+    )
+
+    assert result.exit_code == 1
+    assert "Error connecting to jira." in result.output
+
+
+def test_jira_issue_error(mock_ini, mock_commit_msg_file, monkeypatch, cli_runner):
+    """Assert error on jira issue api call"""
+    mock_commit_msg_file.write_text("TAG-123")
+
+    def mock_init(*args, **kwargs):
+        pass
+
+    def mock_issue(*args, **kwargs):
+        raise jira_exc.JIRAError()
+
+    monkeypatch.setattr(JIRA, "__init__", mock_init)
+    monkeypatch.setattr(JIRA, "issue", mock_issue)
+
+    result = cli_runner.invoke(
+        main, ["--jira-tag=TAG", "--jira-url=URL", str(mock_commit_msg_file)]
+    )
+
+    assert result.exit_code == 1
+    assert "TAG-123 does not exist or no permission" in result.output
+
+
+def test_no_verify(mock_ini, mock_commit_msg_file, cli_runner):
+    """Assert no error on --no-verify"""
+    mock_commit_msg_file.write_text("TAG-123")
+
+    result = cli_runner.invoke(
+        main, ["--jira-tag=TAG", "--no-verify", str(mock_commit_msg_file)]
+    )
+
+    assert result.exit_code == 0
