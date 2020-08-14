712fa421e30c7bdf69f338683a39f42fb8bdb62f
Cielquan <cielquan@protonmail.com>
2020-08-14 15:17:48 +0200

    update pre-commit config

--- .tox_env_exe_runner.py
+++ .tox_env_exe_runner.py
@@ -1,22 +1,18 @@
-#!/usr/bin/env python3
-"""Script to call executables in `tox` envs
-
+"""Script to call executables in `tox` envs.
 The script takes two mandatory arguments:
 1. the executable to call like e.g. `pylint`
 2. a string with comma separated `tox` envs to check for the executable
-
 All other arguments after are passed to the tool on call.
-
 The script considers OS and calls the tool accordingly.
 """
-import subprocess
+import subprocess  # nosec
 import sys
 
 from pathlib import Path
 
 
 def main():
-    """Call given `tool` form given `tox` env"""
+    """Call given `tool` from given `tox` env."""
     tool = sys.argv[1]
 
     if sys.platform == "win32":
@@ -40,7 +36,7 @@ def main():
         )
         return 1
 
-    return subprocess.call(cmd)
+    return subprocess.call(cmd)  # nosec
 
 
 if __name__ == "__main__":
