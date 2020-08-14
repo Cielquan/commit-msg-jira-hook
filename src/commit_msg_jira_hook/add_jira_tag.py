fd244c171638777b0eeb5f5dd48308d989b2103f
Cielquan <cielquan@protonmail.com>
2020-08-14 19:52:31 +0200

    add jira_prepare_commit_msg hook

---
+++ src/commit_msg_jira_hook/add_jira_tag.py
@@ -0,0 +1,104 @@
+# noqa: D205,D208,D400
+"""
+    commit_msg_jira_hook.add_jira_tag
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    Prepend issue tag to commit message.
+
+    :copyright: (c) 2020 Christian Riedel
+    :license: GPLv3, see LICENSE for more details
+"""
+import re
+import sys
+
+from pathlib import Path
+
+import click
+
+from git import Repo  # type: ignore[import]
+
+
+@click.command()
+@click.option(
+    "--always/--auto",
+    default=False,
+    help=(
+        "Switch between always adding JIRA issue in front or only adding if missing. "
+        "Default=auto"
+    ),
+)
+@click.option(
+    "--deactivate-with",
+    type=str,
+    help=(
+        "Don't add JIRA issue when commit message starts with given string. "
+        "Instead remove the string from the commit message."
+    ),
+)
+@click.option(
+    "--no-error",
+    type=bool,
+    is_flag=True,
+    help=(
+        "Exit with exit code 0 when no JIRA issue can be found in current branch name."
+    ),
+)
+@click.option(
+    "--jira-tag", required=True, type=str, help="TAG of jira project. [Mandatory]"
+)
+@click.argument("commit-msg-file", required=True, nargs=1)
+@click.pass_context
+def main(  # pylint: disable=too-many-arguments
+    ctx,
+    jira_tag: str,
+    deactivate_with: str,
+    always: bool,
+    no_error: bool,
+    commit_msg_file: str,
+) -> None:
+    """Prepend commit msg with JIRA issue number.
+
+    Extract JIRA issue number from current branch name and
+    add it the front of your commit message.
+
+    COMMIT_MSG_FILE: Path to file with commit-msg. Passed by pre-commit."
+    """
+    #: Get commit msg
+    with open(commit_msg_file) as cm_file:
+        c_msg = cm_file.read()
+
+    #: Exit if commit msg beginns with deactivate_with string
+    if deactivate_with and c_msg and c_msg.startswith(deactivate_with):
+        #: Remove deactivate_with string
+        with open(commit_msg_file, mode="w") as cm_file:
+            cm_file.write(c_msg.lstrip(deactivate_with).lstrip())
+        ctx.exit()
+
+    #: JIRA issue regex
+    jira_issue_re = re.compile(r"(" + jira_tag.upper() + r")-?([0-9]+)?")
+
+    #: Get JIRA issue from current branch
+    extract = jira_issue_re.search(Repo(Path.cwd()).active_branch.name)
+
+    #: Exit if no issue found in branch name
+    if not extract:
+        if no_error:
+            ctx.exit()
+        click.echo(
+            "JIRA tag with issue number could not be found in current branch name."
+        )
+        ctx.abort()
+
+    branch_jira_issue = extract.group(0)  # type: ignore[union-attr]
+
+    #: Exit if issue already in commit msg
+    if jira_issue_re.search(c_msg) and not always:
+        ctx.exit()
+
+    #: Update commit msg
+    with open(commit_msg_file, mode="w") as cm_file:
+        cm_file.write(f"{branch_jira_issue}: {c_msg}")
+
+
+if __name__ == "__main__":
+    sys.exit(main())  # pylint: disable=E1120
---
+++ tests/test_add_jira_tag.py
@@ -0,0 +1,125 @@
+# noqa: D205,D208,D400
+"""
+    tests.test_add_jira_tag
+    ~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    test add_jira_tag
+
+    :copyright: (c) 2020 Christian Riedel
+    :license: GPLv3, see LICENSE for more details
+"""
+# pylint: disable=W0613,W0621
+from pathlib import Path
+
+import pytest
+
+from click.testing import CliRunner
+from git import Repo  # type: ignore[import]
+
+from commit_msg_jira_hook.add_jira_tag import main
+
+
+@pytest.fixture
+def cli_runner():
+    """Yield click cli test runner."""
+    yield CliRunner()
+
+
+@pytest.fixture
+def mock_home(monkeypatch, tmp_path):
+    """Yield `home` dir mock path."""
+    home = tmp_path / "~"
+    home.mkdir()
+    monkeypatch.setattr(Path, "home", lambda: home)
+    yield home
+
+
+@pytest.fixture
+def mock_commit_msg_file(mock_home):
+    """Yield 'COMMIT_MSG' path in `home` dir mock."""
+    commit_msg_file = mock_home / "COMMIT_MSG"
+    yield commit_msg_file
+
+
+def test_deactivate_with(cli_runner, mock_commit_msg_file):
+    """Assert commit msg w/o prepended string."""
+    mock_commit_msg_file.write_text("no-tag commit message 0815")
+    result = cli_runner.invoke(
+        main, ["--jira-tag=TAG", "--deactivate-with=no-tag", str(mock_commit_msg_file)]
+    )
+    assert result.exit_code == 0
+    assert mock_commit_msg_file.read_text() == "commit message 0815"
+
+
+def test_abort_no_tag_in_branch_name(cli_runner, mock_commit_msg_file, monkeypatch):
+    """Assert exit code 1 if no tag found in branch name."""
+
+    class MockBranch:  # pylint: disable=too-few-public-methods, missing-class-docstring
+        name = "no-tag-in-branch-name"
+
+    monkeypatch.setattr(Repo, "active_branch", MockBranch())
+
+    mock_commit_msg_file.write_text("DUMMY")
+    result = cli_runner.invoke(main, ["--jira-tag=TEST", str(mock_commit_msg_file)])
+    assert result.exit_code == 1
+
+
+def test_exit_no_tag_in_branch_name(cli_runner, mock_commit_msg_file, monkeypatch):
+    """Assert exit code 0 if no tag found in branch name."""
+
+    class MockBranch:  # pylint: disable=too-few-public-methods, missing-class-docstring
+        name = "no-tag-in-branch-name"
+
+    monkeypatch.setattr(Repo, "active_branch", MockBranch())
+
+    mock_commit_msg_file.write_text("DUMMY")
+    result = cli_runner.invoke(
+        main, ["--jira-tag=TEST", "--no-error", str(mock_commit_msg_file)]
+    )
+    assert result.exit_code == 0
+
+
+def test_tag_in_msg_add_again(cli_runner, mock_commit_msg_file, monkeypatch):
+    """Assert tag is always added in front."""
+
+    class MockBranch:  # pylint: disable=too-few-public-methods, missing-class-docstring
+        name = "feat/TEST-123-branch"
+
+    monkeypatch.setattr(Repo, "active_branch", MockBranch())
+
+    mock_commit_msg_file.write_text("TEST-123 commit message 0815")
+    result = cli_runner.invoke(
+        main, ["--jira-tag=TEST", "--always", str(mock_commit_msg_file)]
+    )
+    assert result.exit_code == 0
+    assert mock_commit_msg_file.read_text() == "TEST-123: TEST-123 commit message 0815"
+
+
+def test_tag_in_msg_not_add_again(cli_runner, mock_commit_msg_file, monkeypatch):
+    """Assert tag is not added again."""
+
+    class MockBranch:  # pylint: disable=too-few-public-methods, missing-class-docstring
+        name = "feat/TEST-123-branch"
+
+    monkeypatch.setattr(Repo, "active_branch", MockBranch())
+
+    mock_commit_msg_file.write_text("TEST-123 commit message 0815")
+    result = cli_runner.invoke(
+        main, ["--jira-tag=TEST", "--auto", str(mock_commit_msg_file)]
+    )
+    assert result.exit_code == 0
+    assert mock_commit_msg_file.read_text() == "TEST-123 commit message 0815"
+
+
+def test_tag_added(cli_runner, mock_commit_msg_file, monkeypatch):
+    """Assert normal use works."""
+
+    class MockBranch:  # pylint: disable=too-few-public-methods, missing-class-docstring
+        name = "feat/TEST-123-branch"
+
+    monkeypatch.setattr(Repo, "active_branch", MockBranch())
+
+    mock_commit_msg_file.write_text("commit message 0815")
+    result = cli_runner.invoke(main, ["--jira-tag=TEST", str(mock_commit_msg_file)])
+    assert result.exit_code == 0
+    assert mock_commit_msg_file.read_text() == "TEST-123: commit message 0815"
